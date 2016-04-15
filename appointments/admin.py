1eb8ec4ff354ebf34c266d45e8c29a0c833af33e
Anna Kurylo <anna.kurilo@outlook.com>
2016-04-15 16:23:38 +0300

    Configure filters in admin panel

--- appointments/admin.py
+++ appointments/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Appointment, Doctor
 
 
-admin.site.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ['doctor', 'date', 'timeslot', 'patient_name']
+    list_filter = ['doctor', ]
+
+admin.site.register(Appointment, AppointmentAdmin)
 admin.site.register(Doctor)
