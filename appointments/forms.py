5818aa9ef24fda39b885a7da288df1794de2e835
Anna Kurylo <anna.kurilo@outlook.com>
2016-04-17 00:07:03 +0300

    Add date validation

--- appointments/forms.py
+++ appointments/forms.py
@@ -24,3 +24,13 @@ class AppointmentForm(forms.ModelForm):
                     'startDate': date.today().strftime('%Y-%m-%d'),
                 }),
         }
+
+    def clean_date(self):
+        day = self.cleaned_data['date']
+
+        if day <= date.today():
+            raise forms.ValidationError('Date should be upcoming (tomorrow or later)', code='invalid')
+        if day.isoweekday() in (0, 6):
+            raise forms.ValidationError('Date should be a workday', code='invalid')
+
+        return day
--- appointments/views.py
+++ appointments/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .forms import AppointmentForm
 
 
 def new_appointment(request):
-    form = AppointmentForm()
+    if request.method == 'POST':
+        form = AppointmentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('')
+    else:
+        form = AppointmentForm()
     return render(request, 'appointment_form.html', {'form': form})
