3eecada21569349fdb8c11aead88882983c7b2e9
Anna Kurylo <anna.kurilo@outlook.com>
2016-04-17 01:18:31 +0300

    Add app starting script

---
+++ start.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Run with root!"
+    exit 1
+fi
+
+export DEBIAN_FRONTEND=noninteractive
+
+# TODO: set up nginx+wsgi and normal db
+apt-get install -y python3 nodejs
+
+npm install
+virtualenv -p python3 .env
+source ./.env/bin/activate
+pip install -r requirements.txt
+
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+gunicorn doctor_appointment.wsgi:application -D -b :80 -P gunicorn.pid
+echo "The app is listening to port 80!"
